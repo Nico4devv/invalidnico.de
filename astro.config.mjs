13fb9bea3eec26b26bb1a6702cef84a8423355e2
Nico <117511417+Nico4devv@users.noreply.github.com>
2025-11-13 21:12:54 +0100

    configure Astro project to use Node adapter with custom server settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
-import netlify from '@astrojs/netlify';
-
-// https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: netlify(),
+    adapter: node({
+        mode: 'standalone',
+    }),
+    server: {
+        host: "127.0.0.1",
+        port: 3000,
+    },
+    vite: {
+        server: {
+            allowedHosts: [
+                "invalidnico.de",
+                "www.invalidnico.de",
+                "5.175.245.142",
+                "localhost",
+                "127.0.0.1",
+            ]
+        }
+    }
 })
